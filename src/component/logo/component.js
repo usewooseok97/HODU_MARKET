91900018f6b2238a3434e60f608e4c2c6d7f8eb1
jun <rrrr1133@naver.com>
2026-01-14 12:51:57 +0900

    fixed: logo-component 화질문제 해결

--- src/component/logo/component.js
+++ src/component/logo/component.js
@@ -1,21 +1,47 @@
-// logo.js
-class Logo extends HTMLElement {
+class LogoComponent extends HTMLElement {
   connectedCallback() {
     this.render()
+    this.loadStyles()
   }
 
   render() {
-    // 속성으로 크기, href, class를 받을 수 있도록 설정
-    const width = this.getAttribute('width') || '300'
-    const href = this.getAttribute('href') || ''
-    const linkClass = this.getAttribute('link-class') || 'hodu'
+    // 속성값 가져오기 (기본값 설정)
+    const href = this.getAttribute('href') || '/'
+    const src = this.getAttribute('src') || './src/assets/images/Logo-hodu.png'
+    const alt = this.getAttribute('alt') || 'hodu'
+    const width = this.getAttribute('width') || ''
+    const height = this.getAttribute('height') || ''
+
+    // width, height 속성 문자열 생성
+    const widthAttr = width ? `width="${width}"` : ''
+    const heightAttr = height ? `height="${height}"` : ''
 
     this.innerHTML = `
-      <a href="${href}" class="${linkClass}" style="display: inline-block; width: ${width}px;">
-        <img src="/src/assets/images/Logo-hodu.png" alt="Hodu" style="width: 100%; height: auto; display: block;" />
+      <a href="${href}" class="logo-link">
+        <img src="${src}" alt="${alt}" class="logo-img" ${widthAttr} ${heightAttr} />
       </a>
     `
   }
+
+  // 속성 변경 감지
+  static get observedAttributes() {
+    return ['href', 'src', 'alt', 'width', 'height']
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.render()
+    }
+  }
+
+  loadStyles() {
+    if (!document.querySelector('link[href*="logo/styles.css"]')) {
+      const link = document.createElement('link')
+      link.rel = 'stylesheet'
+      link.href = '/src/component/logo/styles.css'
+      document.head.appendChild(link)
+    }
+  }
 }
 
-customElements.define('logo-component', Logo)
+customElements.define('logo-component', LogoComponent)
--- src/component/logo/youtube copy 4.js
+++ src/component/logo/youtube copy 4.js
@@ -1,25 +1,36 @@
-class YoutubeComponent extends HTMLElement {
+class RadioComponent extends HTMLElement {
   connectedCallback() {
     this.render()
     this.loadStyles()
   }
 
   render() {
+    // checked 속성이 있으면 radio-on, 없으면 radio-off
+    const radioClass = this.hasAttribute('checked') ? 'radio-on' : 'radio-off'
     this.innerHTML = `
-      <a href="" class="youtube">
-        <img src="./src/assets/images/icon-yt.png" alt="Youtube" />
-      </a>
+      <div class="${radioClass}"></div>
     `
   }
 
+  // checked 속성이 변경될 때 호출됨
+  static get observedAttributes() {
+    return ['checked']
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'checked') {
+      this.render()
+    }
+  }
+
   loadStyles() {
-    if (!document.querySelector('link[href*="logo/styles.css"]')) {
+    if (!document.querySelector('link[href*="radio/styles.css"]')) {
       const link = document.createElement('link')
       link.rel = 'stylesheet'
-      link.href = '/src/component/logo/styles.css'
+      link.href = '/src/component/radio/styles.css'
       document.head.appendChild(link)
     }
   }
 }
 
-customElements.define('logo-youtube', YoutubeComponent)
+customElements.define('radio-component', RadioComponent)
