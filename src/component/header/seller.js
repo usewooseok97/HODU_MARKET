41e033498290fe3329afa31cb1c4428a33f146a2
KJH1208 <guswnrkd12@gmail.com>
2026-01-14 14:13:31 +0900

    feat: 판매자 센터 헤더 컴포넌트 및 스타일 추가

---
+++ src/component/header/seller.js
@@ -0,0 +1,38 @@
+// src/component/header/seller.js
+
+class HeaderSeller extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  render() {
+    this.innerHTML = `
+      <header class="header-seller">
+        <div class="header-seller-container">
+          <div class="header-seller-left-group">
+            <div class="header-seller-logo-wrapper">
+              <logo-component width="80px"></logo-component>
+            </div>
+            <h1 class="header-seller-title">판매자 센터</h1>
+          </div>
+        </div>
+      </header>
+    `
+    this.loadStyles()
+  }
+
+  loadStyles() {
+    if (!document.querySelector('link[href*="header/seller.css"]')) {
+      const link = document.createElement('link')
+      link.rel = 'stylesheet'
+      link.href = '/src/component/header/seller.css'
+      document.head.appendChild(link)
+    }
+  }
+}
+
+customElements.define('header-seller', HeaderSeller)
