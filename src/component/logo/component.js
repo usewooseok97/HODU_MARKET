3bea2c70522529fce107440005e8652c8b759f73
jun <rrrr1133@naver.com>
2026-01-14 11:09:33 +0900

    fixed: logo-component 절대경로로 지정 및 css display:block; 추가

--- src/component/logo/component.js
+++ src/component/logo/component.js
@@ -5,28 +5,16 @@ class Logo extends HTMLElement {
   }
 
   render() {
-    // 속성으로 크기를 받을 수 있도록 설정 (기본값: 300px)
+    // 속성으로 크기, href, class를 받을 수 있도록 설정
     const width = this.getAttribute('width') || '300'
-    const href = this.getAttribute('href') || '/'
+    const href = this.getAttribute('href') || ''
+    const linkClass = this.getAttribute('link-class') || 'hodu'
 
     this.innerHTML = `
-      <div class="logo-box" style="width: ${width}px;">
-        <a href="${href}" class="logo">
-          <img src="./src/assets/images/Logo-hodu.png" alt="Hodu" />
-        </a>
-      </div>
+      <a href="${href}" class="${linkClass}" style="display: inline-block; width: ${width}px;">
+        <img src="/src/assets/images/Logo-hodu.png" alt="Hodu" style="width: 100%; height: auto; display: block;" />
+      </a>
     `
-
-    this.loadStyles()
-  }
-
-  loadStyles() {
-    if (!document.querySelector('link[href*="logo/styles.css"]')) {
-      const link = document.createElement('link')
-      link.rel = 'stylesheet'
-      link.href = '/src/component/logo/styles.css'
-      document.head.appendChild(link)
-    }
   }
 }
 
